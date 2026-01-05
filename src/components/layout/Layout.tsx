e8af88f622aceddae67f4ecde3f3d8359bbbf81d
SanthoshKumar-PS <santhoshkumarsakthi2003@gmail.com>
2026-01-05 20:08:03 +0530

    Solved header overflow issue

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,7 +4,7 @@ import Header from "./Header"
 
 const Layout = ({children} : {children:ReactNode}) => {
   return (
-    <div className="min-h-screen flex flex-col bg-background">
+    <div className="w-full overflow-x-hidden min-h-screen flex flex-col bg-background">
         <Header/>
         <main className="flex-1">{children}</main>
         <Footer/>
